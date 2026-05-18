6d1e1cbd358840859578892a9f6f3f11a0b2cc55
Pranav Reddy Gorla <gorlareddy940@gmail.com>
2026-05-18 18:26:32 +0530

    added new line in main.py and also in feature1.py

--- feature1.py
+++ feature1.py
@@ -1,6 +1,7 @@
 def feature1():
     print("This is feature 1")
     print("This is an additional line for feature 1")
+    print("This is another additional line for feature 1")
 
 if __name__ == "__main__":
     feature1()
